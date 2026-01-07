bd743dcad9ab664bc86b3b9515d86bfa91a81f49
SAAMA-TECH <157895776+SAAMA-TECH@users.noreply.github.com>
2026-01-07 10:05:42 +0530

    Add files via upload

---
+++ exp01.py
@@ -0,0 +1,9 @@
+print("SAYYED AFFAN AHMED - 251P088")
+name =input(" What Is Your Name?")
+hobby = input("What's Your Favorite Hobby?")
+food =input("What's Your Favorite food?")
+
+print("\nHello,"+ name +"!")
+print("It's great you enjoy "+ hobby +".")
+print("I hope you get to eat some "+ food +" today")
+print("Have an amazing day,"+ name +"!")
