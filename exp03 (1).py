149b4ddb7a15b0f430818e4c8c496e52922c0cab
SAAMA-TECH <157895776+SAAMA-TECH@users.noreply.github.com>
2026-01-25 18:36:51 +0530

    Add files via upload

---
+++ exp03 (1).py
@@ -0,0 +1,13 @@
+import math
+print("SAYYED AFFAN AHMED - 251P088")
+basic_salary = float(input("Enter The Basic Salary (BS):"))
+DA = 0.7 * basic_salary
+TA = 0.3 * basic_salary
+HRA = 0.1 * basic_salary 
+gross_salary = basic_salary  + DA + TA + HRA
+print("\nSalary Details:") 
+print("Basic Salary (BS):", basic_salary) 
+print("Dearness Allowance (DA):", DA) 
+print("Travel Allowance (TA):", TA) 
+print("House Rent Allowance (HRA):", HRA) 
+print("Gross Salary:", gross_salary)
