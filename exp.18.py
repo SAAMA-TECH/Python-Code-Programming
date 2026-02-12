a10cc3495971234fa545c09898fce8cf37f61d04
SAAMA-TECH <157895776+SAAMA-TECH@users.noreply.github.com>
2026-02-12 23:25:29 +0530

    Add files via upload

---
+++ exp.18.py
@@ -0,0 +1,9 @@
+print("Sayyed Affan Ahmed - 251P088/13")
+import numpy as np
+arr1 = np.array([10,20,30,40,50])
+print("1D Array:",arr1)
+
+print("First Element:",arr1[0])
+print("last Element:",arr1[-1])
+
+print("Element From Index 1 to 3:",arr1[1:4])
